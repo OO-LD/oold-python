a221e11ebb6e4de86cae75aa52046fd12305bbe3
SimonTaurus <simon.stier@gmx.de>
2025-12-14 10:49:30 +0100

    feat: add auth module with example

--- src/oold/backend/sparql.py
+++ src/oold/backend/sparql.py
@@ -5,6 +5,7 @@ from pydantic import ConfigDict
 from rdflib import Graph
 from SPARQLWrapper import JSONLD, SPARQLWrapper
 
+from oold.backend.auth import GetCredentialParam, UserPwdCredential, get_credential
 from oold.backend.interface import Backend, Resolver, StoreResult
 
 
@@ -72,6 +73,60 @@ class LocalSparqlBackend(LocalSparqlResolver, Backend):
 
     def query():
         raise NotImplementedError()
+
+
+class SparqlResolver(Resolver):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    endpoint: str
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._sparql = SPARQLWrapper(self.endpoint)
+
+    def resolve_iris(self, iris: List[str]) -> Dict[str, Dict]:
+        # sparql query to get a node by IRI with all its properties
+        # using CONSTRUCT to get the full node
+        # format the result as json-ld
+        jsonld_dicts = {}
+
+        # lookup  credential for the endpoint
+        cred = get_credential(GetCredentialParam(iri=self.endpoint))
+        if cred is not None:
+            if isinstance(cred, UserPwdCredential):
+                self._sparql.setCredentials(
+                    cred.username, cred.password.get_secret_value()
+                )
+
+        for iri in iris:
+            iri_filter = f"FILTER (?s = {iri})"
+            # check if the iri is a full IRI or a prefix
+            if iri.startswith("http"):
+                iri_filter = f"FILTER (?s = <{iri}>)"
+            self._sparql.setQuery(
+                """
+                PREFIX ex: <https://example.com/>
+                CONSTRUCT {
+                    ?s ?p ?o .
+                }
+                WHERE {
+                    ?s ?p ?o .
+                    {{{iri_filter}}}
+                }
+                """.replace(
+                    "{{{iri_filter}}}", iri_filter
+                )
+            )
+            self._sparql.setReturnFormat(JSONLD)
+            result: Graph = self._sparql.query().convert()
+            if len(result) == 0:
+                jsonld_dicts[iri] = None
+                continue
+            jsonld_dict = json.loads(result.serialize(format="json-ld"))[0]
+            jsonld_dicts[iri] = jsonld_dict
+
+        return jsonld_dicts
 
 
 class WikiDataSparqlResolver(Resolver):
